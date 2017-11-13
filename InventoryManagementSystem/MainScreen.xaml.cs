2fc5b9408fd80a8c9273e3f8695f3f546b74b320
Joe Velasquez <thrawniejoe@gmail.com>
2017-11-13 16:50:23 -0600

    Syncing

--- InventoryManagementSystem/MainScreen.xaml.cs
+++ InventoryManagementSystem/MainScreen.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -71,6 +72,11 @@ namespace InventoryManagementSystem
             inventoryDBDataSetLocationsTableAdapter.Fill(inventoryDBDataSet.Locations);
             System.Windows.Data.CollectionViewSource locationsViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("locationsViewSource")));
             locationsViewSource.View.MoveCurrentToFirst();
+            // Load data into the table Categories. You can modify this code as needed.
+            InventoryManagementSystem.InventoryDBDataSetTableAdapters.CategoriesTableAdapter inventoryDBDataSetCategoriesTableAdapter = new InventoryManagementSystem.InventoryDBDataSetTableAdapters.CategoriesTableAdapter();
+            inventoryDBDataSetCategoriesTableAdapter.Fill(inventoryDBDataSet.Categories);
+            System.Windows.Data.CollectionViewSource categoriesViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("categoriesViewSource")));
+            categoriesViewSource.View.MoveCurrentToFirst();
         }
 
         private void BtnDeleteUser_Click(object sender, RoutedEventArgs e)
@@ -355,9 +361,83 @@ namespace InventoryManagementSystem
                               select o).ToList();
             statusListDataGrid.ItemsSource = LocationList;
         }
-
         //****************************//
         //      END ADMIN LOCATIONS   //
         //****************************//
+
+
+        //****************************//
+        //   Settings - Importer      //
+        //****************************//
+        private void BtnImportInventory_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(TxtFileLink.Text)) //checks to make sure a file has been selected
+            {
+                try
+                {
+                    string filePath = TxtFileLink.Text;
+                    var readcsv = System.IO.File.ReadAllText(filePath);
+
+                    string[] csvfilerecord = readcsv.Split('\n');
+
+                    int countTotal = csvfilerecord.Length;
+                    int count = 0;
+                    //MessageBox.Show(Convert.ToString(csvfilerecord.Length));
+                    foreach (var row in csvfilerecord)
+                    {
+                        if (!string.IsNullOrEmpty(row))
+                        {
+                            count = count + 1;
+                            var cells = row.Split(',');
+                            //MessageBox.Show(cells[0] + ", \n" + cells[1] + cells[2] + ", \n" + cells[3] + ", \n" + cells[4] + ", \n" + cells[5] + ", \n" + cells[6] + ", \n" + cells[7] + ", \n" + cells[8] + ", \n" + cells[9] + ", \n" + cells[10] + ", \n" + cells[11] + ", \n" + cells[12] + ", \n" + cells[13] + ", \n" + cells[14] + ", \n" + cells[15] + ", \n" + cells[16] + ", \n" + cells[17] + ", \n" + cells[18] + ", " + cells[19]);
+
+                            var Inventory = new Inventory //creates a new asset and put's the information from the csv into each field
+                            {
+                                itemName = cells[0],
+                                tag = cells[1],
+                                serialNumber = cells[2],
+                            };
+
+                            //lblDataUpdate.Text = row;
+                            //lblDataUpdate.Refresh();
+                            PbImportProgressBar.Value = (int)(100.0 * count / countTotal);
+
+                            var context = new InventoryManagementSystem.InventoryDBEntities();
+                            using (var dbAsset = new InventoryDBDataSet())
+                            {
+                                context.Inventories.Add(Inventory);
+                                context.SaveChanges();
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    //MessageBox.Show(e.Message);
+                }
+            }
+            else
+            {
+                //MessageBox.Show("Please select a file first");
+            }
+        }
+
+        //Brings up filedialog box and ask for a file.
+        private void getFile(TextBox textbox)
+        {
+            string path;
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Excel Files (*.xlsx)|*.xlsx";
+            file.Multiselect = false;
+            if (file.ShowDialog() == true)
+            {
+                path = file.FileName;
+                textbox.Text = path;
+            }
+        }
+        //****************************//
+        //    END SETTINGS IMPORTER   //
+        //****************************//
+
     }
 }
--- InventoryManagementSystem/ModelClass/Importer.cs
+++ InventoryManagementSystem/ModelClass/Importer.cs
@@ -10,58 +10,58 @@ namespace InventoryManagementSystem.ModelClass
     {
         //Imports a CSV file into the database
         //Still needs some work
-        private void csvReader()
-        {
-            if (!string.IsNullOrWhiteSpace(txtFilePath.Text)) //checks to make sure a file has been selected
-            {
-                try
-                {
-                    string filePath = txtFilePath.Text;
-                    var readcsv = System.IO.File.ReadAllText(filePath);
+        //private void csvReader()
+        //{
+        //    if (!string.IsNullOrWhiteSpace(txtFilePath.Text)) //checks to make sure a file has been selected
+        //    {
+        //        try
+        //        {
+        //            string filePath = txtFilePath.Text;
+        //            var readcsv = System.IO.File.ReadAllText(filePath);
 
-                    string[] csvfilerecord = readcsv.Split('\n');
+        //            string[] csvfilerecord = readcsv.Split('\n');
 
-                    int countTotal = csvfilerecord.Length;
-                    int count = 0;
-                    //MessageBox.Show(Convert.ToString(csvfilerecord.Length));
-                    foreach (var row in csvfilerecord)
-                    {
-                        if (!string.IsNullOrEmpty(row))
-                        {
-                            count = count + 1;
-                            var cells = row.Split(',');
-                            //MessageBox.Show(cells[0] + ", \n" + cells[1] + cells[2] + ", \n" + cells[3] + ", \n" + cells[4] + ", \n" + cells[5] + ", \n" + cells[6] + ", \n" + cells[7] + ", \n" + cells[8] + ", \n" + cells[9] + ", \n" + cells[10] + ", \n" + cells[11] + ", \n" + cells[12] + ", \n" + cells[13] + ", \n" + cells[14] + ", \n" + cells[15] + ", \n" + cells[16] + ", \n" + cells[17] + ", \n" + cells[18] + ", " + cells[19]);
+        //            int countTotal = csvfilerecord.Length;
+        //            int count = 0;
+        //            //MessageBox.Show(Convert.ToString(csvfilerecord.Length));
+        //            foreach (var row in csvfilerecord)
+        //            {
+        //                if (!string.IsNullOrEmpty(row))
+        //                {
+        //                    count = count + 1;
+        //                    var cells = row.Split(',');
+        //                    //MessageBox.Show(cells[0] + ", \n" + cells[1] + cells[2] + ", \n" + cells[3] + ", \n" + cells[4] + ", \n" + cells[5] + ", \n" + cells[6] + ", \n" + cells[7] + ", \n" + cells[8] + ", \n" + cells[9] + ", \n" + cells[10] + ", \n" + cells[11] + ", \n" + cells[12] + ", \n" + cells[13] + ", \n" + cells[14] + ", \n" + cells[15] + ", \n" + cells[16] + ", \n" + cells[17] + ", \n" + cells[18] + ", " + cells[19]);
 
-                            var Inventory = new Inventory //creates a new asset and put's the information from the csv into each field
-                            {
-                                itemName = cells[0],
-                                tag = cells[1],
-                                serialNumber = cells[2],
-                            };
+        //                    var Inventory = new Inventory //creates a new asset and put's the information from the csv into each field
+        //                    {
+        //                        itemName = cells[0],
+        //                        tag = cells[1],
+        //                        serialNumber = cells[2],
+        //                    };
 
-                            lblDataUpdate.Text = row;
-                            lblDataUpdate.Refresh();
-                            progressBar1.Value = (int)(100.0 * count / countTotal);
+        //                    lblDataUpdate.Text = row;
+        //                    lblDataUpdate.Refresh();
+        //                    progressBar1.Value = (int)(100.0 * count / countTotal);
 
-                            var context = new InventoryManagementSystem.InventoryDBEntities();
-                            using (var dbAsset = new InventoryDBDataSet())
-                            {
-                                context.Inventories.Add(Inventory);
-                                context.SaveChanges();
-                            }
-                        }
-                    }
-                }
-                catch (Exception e)
-                {
-                    //MessageBox.Show(e.Message);
-                }
-            }
-            else
-            {
-                //MessageBox.Show("Please select a file first");
-            }
-        }
+        //                    var context = new InventoryManagementSystem.InventoryDBEntities();
+        //                    using (var dbAsset = new InventoryDBDataSet())
+        //                    {
+        //                        context.Inventories.Add(Inventory);
+        //                        context.SaveChanges();
+        //                    }
+        //                }
+        //            }
+        //        }
+        //        catch (Exception e)
+        //        {
+        //            //MessageBox.Show(e.Message);
+        //        }
+        //    }
+        //    else
+        //    {
+        //        //MessageBox.Show("Please select a file first");
+        //    }
+        //}
 
 
 
